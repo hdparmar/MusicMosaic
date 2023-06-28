c1e65e4e8f4e2887d49536b31ceb1c0cc81f4a4f
Harshdeep Parmar <71045029+hdparmar@users.noreply.github.com>
2023-06-28 02:37:22 +0200

    update index.html

---
+++ routes/search.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/:query', function(req, res, next) {
+  // handle search functionality here
+});
+
+module.exports = router;
